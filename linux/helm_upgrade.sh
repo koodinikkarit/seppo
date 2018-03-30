8e31059e51a880d6f94f4638367cdec9ba75e944
jaska <pusanteemu@gmail.com>
2018-03-30 17:19:50 +0300

    b

--- linux/helm_upgrade.sh
+++ linux/helm_upgrade.sh
@@ -2,6 +2,13 @@ IMAGE_TAG="$TRAVIS_BRANCH-$TRAVIS_COMMIT"
 
 RELEASE_NAME=$(echo "$TRAVIS_BRANCH" | sed 's/\./-/g')
 
+echo $IMAGE_TAG
+echo $MYSQL_USERNAME
+echo $MYSQL_DATABASE
+echo $MYSQL_PASSWORD
+echo $MYSQL_HOST
+echo "seppo-$RELEASE_NAME"
+
 helm upgrade \
 --wait \
 --set seppoImage=jaska/seppo:$IMAGE_TAG \
